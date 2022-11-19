32046d8bd229246ba5ac87bbc273a5945275ce20
833FCAK3 <stiwl@mail.ru>
2022-11-19 13:25:32 +0500

    Add basic pages

--- devsearch/projects/views.py
+++ devsearch/projects/views.py
@@ -1,3 +1,10 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def projects(request):
+    return HttpResponse("Here are our projects")
+
+
+def project(request, pk):
+    return HttpResponse(f"SINGLE PROJECT {pk}")
