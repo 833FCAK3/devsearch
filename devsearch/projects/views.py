8bc3c560eebf2b5da871cd3acd5b6f3e9c739ebd
833FCAK3 <stiwl@mail.ru>
2022-12-03 02:15:46 +0500

    Finish CRUD functionality

--- devsearch/projects/views.py
+++ devsearch/projects/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
 from .forms import ProjectForm
 from .models import Project
@@ -13,11 +13,40 @@ def projects(request):
 
 def project(request, pk):
     project_obj = Project.objects.get(id=pk)
-    tags = project_obj.tags.all()
     return render(request, "projects/single-project.html", {"project": project_obj})
 
 
 def create_project(request):
     form = ProjectForm()
+
+    if request.method == "POST":
+        form = ProjectForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("projects")
+
     context = {"form": form}
     return render(request, "projects/project_form.html", context)
+
+
+def update_project(request, pk):
+    project = Project.objects.get(id=pk)
+    form = ProjectForm(instance=project)
+
+    if request.method == "POST":
+        form = ProjectForm(request.POST, instance=project)
+        if form.is_valid():
+            form.save()
+            return redirect("projects")
+
+    context = {"form": form}
+    return render(request, "projects/project_form.html", context)
+
+
+def delete_project(request, pk):
+    project = Project.objects.get(id=pk)
+    if request.method == "POST":
+        project.delete()
+        return redirect("projects")
+    context = {"object": project}
+    return render(request, "projects/delete_template.html", context)
