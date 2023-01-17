f96325e2034d4508556e1bae1e0c04dbab5e2b04
833FCAK3 <stiwl@mail.ru>
2023-01-18 00:15:43 +0500

    Add user account page

--- devsearch/users/models.py
+++ devsearch/users/models.py
@@ -4,9 +4,6 @@ from django.contrib.auth.models import User
 from django.db import models
 
 
-# Create your models here.
-
-
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
     name = models.CharField(max_length=200, null=True, blank=True)
--- devsearch/users/views.py
+++ devsearch/users/views.py
@@ -73,8 +73,19 @@ def profiles(request: WSGIRequest):
 def user_profile(request: WSGIRequest, pk):
     profile: Profile = Profile.objects.get(id=pk)
 
-    topSkills = profile.skill_set.exclude(description__exact="")
-    otherSkills = profile.skill_set.filter(description__exact="")
+    top_skills = profile.skill_set.exclude(description__exact="")
+    other_skills = profile.skill_set.filter(description__exact="")
 
-    context = {"profile": profile, "topSkills": topSkills, "otherSkills": otherSkills}
+    context = {"profile": profile, "topSkills": top_skills, "otherSkills": other_skills}
     return render(request, "users/user-profile.html", context)
+
+
+@login_required(login_url="login")
+def user_account(request: WSGIRequest):
+    profile: Profile = request.user.profile
+
+    skills = profile.skill_set.all()
+    projects = profile.project_set.all()
+
+    context = {"profile": profile, "skills": skills, "projects": projects}
+    return render(request, "users/account.html", context)
